59c265d6fc6a8aa70e8d959fa87834d23d06f616
FreddyVilla94 <fevig1994@gmail.com>
2017-08-25 16:06:20 -0600

    subiendo

---
+++ houndify-sdk-sample-0.2.22/src/main/java/com/houndify/sample/Constants.java
@@ -0,0 +1,9 @@
+package com.houndify.sample;
+
+/**
+ * General constants.
+ */
+public class Constants {
+    public static final String CLIENT_ID = "GFLMK5iB9VYzb1W83lhcNw==";
+    public static final String CLIENT_KEY = "5pWCGkpeIkrFee76slEGO6ZSogmFFBkaYU8mBUc1NQe0mvovUy_yb22x7t7YjEU0pqeefUC3YzmGf9dn060sIA==";
+}
